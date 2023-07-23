d8f55cef6560a71fb4f27224a7b83dd69c49c8d4
jkhallpurpose <jkhallpurpose@gmail.com>
2023-07-23 19:16:38 -0400

    1st

---
+++ app/api/listings/route.ts
@@ -0,0 +1,50 @@
+import Server from "next/server";
+
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export async function POST(
+  request: Request, 
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return Server.NextResponse.error();
+  }
+
+  const body = await request.json();
+  const { 
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+   } = body;
+
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) {
+      Server.NextResponse.error();
+    }
+  });
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id
+    }
+  });
+
+  return Server.NextResponse.json(listing);
+}
---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,37 @@
+import Server from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  reservationId?: string;
+}
+
+export async function DELETE(
+  request: Request, 
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return Server.NextResponse.error();
+  }
+
+  const { reservationId } = params;
+
+  if (!reservationId || typeof reservationId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  const reservation = await prisma.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [
+        { userId: currentUser.id },
+        { listing: { userId: currentUser.id } }
+      ]
+    }
+  });
+
+  return Server.NextResponse.json(reservation);
+}
